d1d8e0d385cf41fee4295e42a2a05f3e7887766d
AseelHussein96 <aseelayed1996@gmail.com>
2021-02-28 20:40:01 +0200

    lab01

---
+++ demo/js/app.js
@@ -0,0 +1,21 @@
+alert('Welcome to Anime World');
+let ask=confirm('Are you sure you want to enter the anime world');
+
+
+if (ask==true) {
+    x=" smart choice";
+    
+let animename=prompt('What is your favorite anime scene :');
+let animecharacter=prompt('If you could meet an anime character who would it be?');
+let soundtrack=prompt('What is your favorite anime soundtrack?');
+let thing =prompt ('What is your favorite thing about anime?');
+alert ('this is one'+animename+'of my favorite anime \n i guess ,me too! i which to meet'+animecharacter+'and every single character have own fascination,\n'+soundtrack+'maybe i dont know this sound track but cause its your favourite it will be mine too ;),\n I know its hard to tell one thing why you like the anime but'+thing+'its perfect');
+
+} else{
+    x=" You lose a lot "
+
+}
+alert(x);
+
+
+
